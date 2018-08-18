a6f7917a21b15783e8486c03af802c0750d5ef6c
Andrew Gunderman <agunderman10@gmail.com>
2018-08-18 15:17:50 -0400

    -Finished most of UI -Adding different needed windows -Added functionality for multiple buttons

--- GunderMarket/MainWindow.xaml.cs
+++ GunderMarket/MainWindow.xaml.cs
@@ -9,9 +9,15 @@ namespace GunderMarket
     public partial class MainWindow : Window
     {
 
+        //holds all needed variables
         #region Private Members
 
-        private bool isLoggedIn;
+        //tells if the user is logged into their account
+        private bool isLoggedIn = false;
+
+
+        //tells if the user has intitiated a new purchase
+        private bool isPurchaseEnded = true;
 
         #endregion
 
@@ -19,12 +25,89 @@ namespace GunderMarket
 
         public MainWindow()
         {
+
             InitializeComponent();
+
+            NewPurchase();
+
         }
 
         #endregion
 
+        #region ButtonClicks
 
+        /// <summary>
+        /// tells what happens when buttons are clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Button_Clicks(object sender, RoutedEventArgs e)
+        {
 
+            //casts sender to a button
+            Button button = (Button)sender;
+
+            //switches through button.Content to learn which button is clicked and the following events
+            switch(button.Content)
+            {
+
+                case "Login":
+                    LoginPage loginPage = new LoginPage();
+                    loginPage.Show();
+                    break; 
+
+                case "Create an Account":
+                    CreateAccountPage createAccountPage = new CreateAccountPage();
+                    createAccountPage.Show();
+                    break;
+
+                case "Deposit":
+                    DepositPage depositPage = new DepositPage();
+                    depositPage.Show();
+                    break;
+
+                case "Withdraw":
+                    WithdrawPage withdrawPage = new WithdrawPage();
+                    withdrawPage.Show();
+                    break;
+
+            }
+
+        }
+
+        #endregion
+
+        #region NewLogin
+
+        /// <summary>
+        /// called when button is clicked
+        /// </summary>
+        public void NewLogin()
+        {
+
+        }
+
+        #endregion
+
+        #region NewPurchase
+
+        /// <summary>
+        /// runs if user starts new purchase
+        /// </summary>
+        public void NewPurchase()
+        {
+
+            //tells that a new purchase has been started
+            isPurchaseEnded = false;
+
+            //if user is not logged in, Panel cannot be accessed
+            if (isLoggedIn == false)
+            {
+                Panel.IsEnabled = false;
+            }
+
+        }
+
+        #endregion
     }
 }
