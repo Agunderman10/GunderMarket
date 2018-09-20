cc99f3f232f628b116d7b2d327729cf00523fa55
Andrew Gunderman <agunderman10@gmail.com>
2018-09-20 08:35:56 -0400

    -Refactor

--- GunderMarket/LoginPage.xaml.cs
+++ GunderMarket/LoginPage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Documents;
+﻿using System.Windows;
 
 namespace GunderMarket
 {
@@ -10,45 +7,9 @@ namespace GunderMarket
     /// </summary>
     public partial class LoginPage : Window
     {
-        public string enteredUserName;
-        public string enteredPassword;
-
         public LoginPage()
         {
             InitializeComponent();
-        }
-
-        public void Login_Click(object sender, RoutedEventArgs e)
-        {
-            //creates variable for user created username
-            enteredUserName = UsernameTextBox.Text;
-
-            //creates variable for user created password
-            enteredPassword = PasswordTextBox.Text;
-
-            //holds all usernames
-            List<string> usernameList = new List<string>
-            {
-                "admin",
-                //userUserName //adds user created username to ArrayList that contains usernames
-            };
-
-            //holds all passwords
-            List<string> passwordList = new List<string>
-            {
-                "12345",
-                //userPassword //adds user created password to ArrayList that contains passwords
-            };
-
-            //if entered password and username are found in the username and password lists and have matching indexes,
-            //then user is able to login.
-            if ((usernameList.Contains(enteredUserName) && passwordList.Contains(enteredPassword)) && 
-                (usernameList.IndexOf(enteredUserName) == (passwordList.IndexOf(enteredPassword))))
-            {
-                Close();
-                MainWindow mw = new MainWindow();
-                mw.IsLoggedIn = true;
-            }
         }
     }
 }
--- GunderMarket/MainWindow.xaml.cs
+++ GunderMarket/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.Collections;
-using System.Windows.Controls;
 
 namespace GunderMarket
 {
@@ -9,112 +7,9 @@ namespace GunderMarket
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        //holds all needed variables
-        #region Private Members
-
-        LoginPage loginPage = new LoginPage();
-
-        public bool IsLoggedIn { get; set; }
-
-        //user's balance
-        private double userBalance;
-
-        #endregion
-
-        #region Initial Constructor
-
         public MainWindow()
         {
-
             InitializeComponent();
-            IsLoggedIn = false; //delete if we run into problems
-
         }
-
-        #endregion
-
-        #region ButtonClicks
-
-        /// <summary>
-        /// tells what happens when buttons are clicked
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Button_Clicks(object sender, RoutedEventArgs e)
-        {
-
-            //casts sender to a button
-            Button button = (Button)sender;
-
-            //switches through button.Content to learn which button is clicked and decides which actions shall occur
-            switch (button.Content)
-            {
-
-                case "Login":
-
-                    if (IsLoggedIn == false) //if user is not logged in, open login page
-                        loginPage.Show();
-                    else if (IsLoggedIn == true)
-                        NeedToLoginMessage();
-                    break;
-
-                case "Create an Account":
-
-                    if (IsLoggedIn == false) { //if user is not logged in, open create account page
-                    CreateAccountPage createAccountPage = new CreateAccountPage();
-                    createAccountPage.Show();
-                    }
-                    else if (IsLoggedIn == true)
-                        NeedToLoginMessage();
-
-                    break;
-
-                case "Deposit":
-
-                    if (IsLoggedIn == true) { //if user is logged in, open deposit page
-                    DepositPage depositPage = new DepositPage();
-                    depositPage.Show();
-                    }
-                    else if (IsLoggedIn == false)
-                        NeedToLoginMessage();
-
-                    break;
-
-                case "Withdraw":
-
-                    if (IsLoggedIn == true) { //if user is logged in, open withdraw page
-                    WithdrawPage withdrawPage = new WithdrawPage();
-                    withdrawPage.Show();
-                    }
-                    else if (IsLoggedIn == false)
-                        NeedToLoginMessage();
-
-                    break;
-
-                case "Clear Order":
-                    break;
-
-                case "Logout":
-
-                    break;
-
-            }
-
-        }
-
-        #endregion
-
-        #region NeedToLoginMessage
-
-        public void NeedToLoginMessage()
-        {
-            if (IsLoggedIn == false)
-                MessageBox.Show("You are not logged in. Please login.");
-            else if (IsLoggedIn == true)
-                MessageBox.Show("You are already logged in!");
-        }
-
-        #endregion
     }
 }
